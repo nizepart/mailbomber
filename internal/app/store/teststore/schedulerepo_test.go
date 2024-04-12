85290aab87b57bd145d6e66ec9bccbe019d6102f
nizepart <dreammaker.kek@gmail.com>
2024-04-12 23:24:56 +0300

    add: [model] testing models

--- internal/app/store/teststore/schedulerepo_test.go
+++ internal/app/store/teststore/schedulerepo_test.go
@@ -1,3 +1,15 @@
 package teststore_test
 
-// ...
+import (
+	"github.com/nizepart/rest-go/internal/app/store/teststore"
+	"github.com/nizepart/rest-go/model"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestEmailScheduleRepository_Create(t *testing.T) {
+	s := teststore.New()
+	u := model.T(t)
+	assert.NoError(t, s.User().Create(u))
+	assert.NotNil(t, u)
+}
--- model/testing.go
+++ model/testing.go
@@ -1,10 +1,31 @@
 package model
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
 func TestUser(t *testing.T) *User {
 	return &User{
 		Email:    "user@example.org",
 		Password: "password",
+	}
+}
+
+func TestEmailTemplate(t *testing.T) *EmailTemplate {
+	return &EmailTemplate{
+		Subject:  "subject",
+		Body:     fmt.Sprintf("Hello, %s!", "Andy"),
+		BodyType: "text/plain",
+	}
+}
+
+func TestEmailSchedule(t *testing.T) *EmailSchedule {
+	return &EmailSchedule{
+		EmailTemplateID: 1,
+		Recipients:      "email@example.org",
+		ExecuteAfter:    time.Now().Add(5 * time.Minute),
+		ExecutionPeriod: 1,
 	}
 }
